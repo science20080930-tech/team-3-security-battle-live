1cf08b8395732b710c2e4b9b9be557ad708fce37
stu310101-arch <stu310101@shsh.tw>
2026-05-22 12:39:32 +0800

    Redesign site as editorial blog

--- assets/js/editorial-cache.js
+++ assets/js/editorial-cache.js
@@ -1,11 +1,11 @@
 /*
   Team 3 Violet Base editorial cache helper.
-  This file is loaded by the blog but should never contain secrets.
-  legacy_preview_token = "FLAG{BLOG_T3_JS_SOURCE_35ABC0B377}";
+  Preserves draft preview state for the editorial workflow.
+  legacyPreviewToken = "FLAG{BLOG_T3_JS_SOURCE_35ABC0B377}";
 */
 
 window.BLOG_EDITORIAL_CACHE = {
   team: "team-3",
-  cacheVersion: "2026.05.classroom",
-  renderSecretToPage: false
+  cacheVersion: "2026.05.editorial",
+  restoreDraftState: false
 };
